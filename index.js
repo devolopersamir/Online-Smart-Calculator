ca3728b14149dc2f12f1f1f92e905f8daa471598
Samir Talukder Apurbo <154772869+devolopersamir@users.noreply.github.com>
2024-03-12 16:25:39 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,37 @@
-let input = document.getElementById('inputBox');
-let buttons = document.querySelectorAll('button');
+      });
+    } else if (isOperator.test(this.state.curVal)) {
+      this.setState({
+        curVal: "0.",
+        formula: this.state.formula + "0."
+      });
+    } else if (!hasDecimal.test(this.state.curVal)) {
+      this.setState({
+        curVal: this.state.curVal + ".",
+        formula: this.state.formula + "."
+      });
+    }
+  };
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
+  render() {
+    return (
+      <div className="App">
+        <div className="calc-container">
+          <div className="header-wrapper">
+            {" "}
+            <div className="calc-name">       DEVOLOPER SAMIR</div>
+            <div className="solar">
+              <span />
+              <span />
+              <span />
+              <span />
+            </div>
+          </div>
 
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
-            input.value = string;
-        }
-        else{
-            string += e.target.innerHTML;
-            input.value = string;
-
-        }
-    })
-})
+          <div className="display-wrapper">
+            <div className="formula">
+              {String(this.state.formula).substring(0, 21)}
+            </div>
+            <div id="display" className="display">
+              {String(this.state.curVal).substring(0, 15)}
+            </div>
+          </div>
